b7a1181b47b37b958691469fdfd01553dba8714b
Tim Maina <oneuptim@Tims-MacBook-Pro.local>
2016-11-27 02:53:15 -0800

    Added application js file and jquery functions

---
+++ js/applications.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+  function grabApplications(){
+
+    for(var i = 0; i < 6; i++){
+
+      $.get(`https://jsonplaceholder.typicode.com/posts/${i}`, function(res){
+
+        var content_id = res.id;
+        var title = res.title;
+        var description = res.body;
+        var html_string = "";
+
+            // html_string += '<h1>' + content_id + '</h1>';
+            html_string += '<h2>' + content_id + " " + title + '</h2>';
+            html_string += '<p>' + description + '</p>'
+
+            $('.api-data').append(html_string);
+
+
+        console.log('I am the description', description);
+        console.log('I am the title', title);
+
+
+      }, 'json')
+    }
+
+    // grabApplications()
+
+  }
+
+
+
+  $('#appBtn').click(function(){
+    $('tbody').append(`<tr><td>11/27/2016</td><td>Las Vegas, NV</td><td>Timothy Maina</td><td>3045 Cobble Creek Court, Las Vegas NV 89108</td><td>$1,000</td><td>Completed</td><td>Processing</td></tr>`);
+
+        console.log("Test Application is working!");
+
+  });
+  // End of append function
+
+
+  $('#apiBtn').click(function(){
+    grabApplications()
+  });
+    // End API Testing
+
+});
+// End of document load function
