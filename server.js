77052d9e23a54388fd5815915e5599c427a03c24
Tim Maina <oneuptim@Tims-MacBook-Pro.local>
2017-06-29 01:31:12 -0600

    Fixed ng bugs

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
-console.log('Server Hit!');
-// server.js
+// Get dependencies
 const express = require('express');
-const app = express();
-// Run the app by serving the static files
-// in the dist directory
-// app.use(express.static(__dirname + '/dist'));
-app.use(express.static(__dirname + '/src'));
-// Start the app by listening on the default
-// Heroku port
-// app.listen(process.env.PORT || 8080);
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
 
-app.listen(4200, function(){
-  console.log("BeltExam App Listening on Port 8000");
+// Get our API routes
+// const api = require('./server/routes/api');
+
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Set our api routes
+// app.use('/api', api);
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
